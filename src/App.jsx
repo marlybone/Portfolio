10b3ab822408fadc61be67e4e212d8a86899d9cb
M <41431476+marlybone@users.noreply.github.com>
2023-01-25 12:54:22 +0000

    content barebones added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import Social from './Components/Social'
+import Content from './Components/Content'
 
 export default function App() {
   return (
     <main>
       <Navbar/>
+      <Content />
       <Social/>
     </main>
   )
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Content = () => {
   return(
-    
+    <div className="content-section">
+      <div className ='content-container'>
+        <div className='items-content'>
+          <h1>Marlon Stevenson</h1>
+          <h2>Frontend Developer</h2>
+          <h3>Javascript | React | CSS | HTML </h3>
+          <p>Description, short, to the point</p>
+          <button>Resume</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
