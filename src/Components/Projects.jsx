77f705f49bfae1f483e59d659c91822de1e1d2ad
M <41431476+marlybone@users.noreply.github.com>
2023-01-25 15:37:55 +0000

    social barebones addded

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return(
-    
+    <div className="grid-container">
+      <div className="grid-items">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+        <div className="current--project">
+          <div className="curr-project">
+          </div>
+        </div>
+    </div>
   )
 }
 
--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Social = () => {
   return(
-    <div>Social</div>
+    <div className="side-container">
+      <div className='side-items'>
+        <a href="">Twitter</a>
+        <a href="">Github</a>
+        <a href="">Linkedin</a>
+        <a href="">Codepen</a>
+      </div>
+    </div>
   )
 }
 
