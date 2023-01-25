dcf9c5aab6badd624a7615d5596938c499fb63f3
M <41431476+marlybone@users.noreply.github.com>
2023-01-25 23:01:17 +0000

    css item placements

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -8,8 +8,8 @@ const Content = () => {
       </div>
       <div className ='content-container'>   
         <div className='items-content'>
-          <h1>Marlon Stevenson</h1>
-          <h2>Frontend Developer</h2>
+          <h1>Marlon Stevenson.</h1>
+          <h2>Frontend Developer.</h2>
           <h3>Javascript | React | CSS | HTML </h3>
           <p>Description, short, to the point</p>
           <button>Resume</button>
