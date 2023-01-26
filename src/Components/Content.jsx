a50269fef6bde83f3b4a2f09f7cff65a0bdabf8e
M <41431476+marlybone@users.noreply.github.com>
2023-01-26 15:57:59 +0000

    content seems positioned

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -5,11 +5,11 @@ const Content = () => {
     <div className="content-section">
       <div className ='content-container'>   
         <div className='items-content'>
-          <img className='img' src="src/img/img.jpg"/>
           <h1>Marlon Stevenson.</h1>
           <h2>Web Developer.</h2>
           <h3>Javascript | React | CSS | HTML </h3>
           <p>Description, short, to the point</p>
+          <img className='img' src="src/img/img.jpg"/>
           <button>Resume</button>
         </div>
       </div>
