c85a729d3405a7e92b7765d768f743970cab8c62
M <41431476+marlybone@users.noreply.github.com>
2023-01-27 23:19:37 +0000

    Going to re-do the grid layout

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,49 +2,9 @@ import React from 'react'
 
 const Projects = () => {
   return(
-  <section>
-  <div className='the-grid'>
-    <h1>Some of my Projects</h1>
-    <div className="grid-container">  
-      <div className="grid-items">
-        <div className="project-card">
-          <img src='' alt=''/>
-            <h3>Project</h3>
-          <div className='project-box'>
-            <p>I built this using google maps API and Placeservices to get tourist attractions within a radius on the searched place.</p>
-          </div>
-            <a>
-              <img/>
-            </a>
-        </div>
-        <div className="project-card">
-          <img src='' alt=''/>
-            <h3>Project</h3>
-            <div className='project-box'>
-            <p>I built this using google maps API and Placeservices to get tourist attractions within a radius on the searched place.</p>
-          </div>
-            <a>
-              <img/>
-            </a>
-        </div>
-        <div className="project-card">
-          <img src='' alt=''/>
-            <h3>Project</h3>
-            <div className='project-box'>
-            <p>I built this using google maps API and Placeservices to get tourist attractions within a radius on the searched place.</p>
-          </div>
-            <a>
-              <img/>
-            </a>
-        </div>
+      <div className='grid-container'>
+        
       </div>
-        <div className="current--project">
-          <div className="curr-project">
-          </div>
-        </div>
-    </div>
-  </div>
-  </section>
   )
 }
 
