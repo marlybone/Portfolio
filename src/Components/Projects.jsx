280c1e623e459cf1b2b0b89cd30d047cd2555c51
M <41431476+marlybone@users.noreply.github.com>
2023-01-28 20:28:07 +0000

    added social font awesome icons

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Projects = () => {
   return(
       <div className='grid-container'>
-        
+        <div className='image-one-col-span-two'></div>
+        <div className='text-col-span-two'></div>
+        <div className='image-two-col-span-two'></div>
+        <div className='text-two-col-span-two'></div>
+        <div className='image-three-col-span-two'></div>
+        <div className='text-three-col-span-two'></div>
       </div>
   )
 }
--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub , faTwitter , faCodepen , faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 
 const Social = () => {
   return(
     <div className="side-container">
       <div className='side-items'>
-        <a href="">Twitter</a>
-        <a href="">Github</a>
-        <a href="">Linkedin</a>
-        <a href="">Codepen</a>
+        <a href=""><FontAwesomeIcon icon={faLinkedin}/></a>
+        <a href=""><FontAwesomeIcon icon={faTwitter}/></a>
+        <a href=""><FontAwesomeIcon icon={faGithub}/></a>
+        <a href=""><FontAwesomeIcon icon={faCodepen}/></a>
       </div>
     </div>
   )
