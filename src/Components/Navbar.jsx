8399d159120c88cfeee0a72857bb49dca1a5dfa7
M <41431476+marlybone@users.noreply.github.com>
2023-01-29 19:56:05 +0000

    working on the grid, picture sizes added some different color to text

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
       <div className="container">
         <div className="wrapper">
           <ul className="nav-items">
-            <a className="items" href="#home">Home</a>
-            <a className="items" href="#about">About</a>
-            <a className="items" href="projects">Projects</a>
-            <a className="items" href="contact">Contact</a>
+            <a className="items" href="#home"> Home </a>
+            <a className="items" href="#about"> About </a>
+            <a className="items" href="projects"> Projects </a>
+            <a className="items" href="contact"> Contact </a>
             <a className="items" href="blog"></a>
           </ul>
         </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Projects = () => {
   return(
       <div className='grid-container'>
-        <div className='image-one-col-span-two'><img src="src/img/img.jpg"/></div>
+        <div className='image-one-col-span-two'><img src="src/img/ss02.jpg"/></div>
         <div className='text-col-span-two'>
         <p>
           This is a project etc etc etc
         </p>
         </div>
-        <div className='image-two-col-span-two'><img src="src/img/img.jpg"/></div>
+        <div className='image-two-col-span-two'><img src="src/img/ss01.jpg"/></div>
         <div className='text-two-col-span-two'>
          <p>
           This is a project etc etc etc
         </p>
         </div>
-        <div className='image-three-col-span-two'><img src="src/img/img.jpg"/></div>
+        <div className='image-three-col-span-two'><img src="src/img/ss03.jpg"/></div>
         <div className='text-three-col-span-two'>
          <p>
           This is a project etc etc etc
