e561709345b4a686c47da875d78fdadd4fd2968c
M <41431476+marlybone@users.noreply.github.com>
2023-01-29 21:17:54 +0000

    grid placement is complete

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,11 +6,12 @@ const Projects = () => {
         <div className='image-one-col-span-two'><img src="src/img/ss02.jpg"/></div>
         <div className='text-col-span-two'>
         <p>
-          This is a project etc etc etc
+          This is a project etc etc etc etc etc It's the tourist app
         </p>
         </div>
         <div className='image-two-col-span-two'><img src="src/img/ss01.jpg"/></div>
         <div className='text-two-col-span-two'>
+          <h1>Project #1</h1>
          <p>
           This is a project etc etc etc
         </p>
