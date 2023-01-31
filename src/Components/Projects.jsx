dfc0fc25d9126baf9ff5186d4deb1af9125c20f7
M <41431476+marlybone@users.noreply.github.com>
2023-01-31 21:22:11 +0000

    added hover item component which does the item bounce and added color on mouseover

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import Imagecycler from './Imagecycler'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import useHoverIcons from './useHoverIcons'
 
 const Projects = () => {
+
   const styles = {
     transform: 'translateX(100%)',
      color: 'white',
     direction: 'rtl'
-  }
-
-  const githubStyle = {
-    fontSize:'2em',
-    color:'white',  
-  }
-  
+  };
   const images = [
     'src/img/ss01.jpg',
     'src/img/ss02.jpg',
     'src/img/ss03.jpg'
-  ]
-  
+  ];
+  const imagestwo = [
+    'src/img/weather01.jpg',
+    'src/img/weather02.jpg'
+  ];
+  useHoverIcons('.github-code');
   return(
     <div>
       <h1 style={{color:'white'}}>My Projects</h1>
@@ -28,17 +28,17 @@ const Projects = () => {
         <div className='image-one-col-span-two'><Imagecycler className='places-proj' images={images}/></div>
         <h1 className='project-tags' style={styles}>Maps PlaceServices</h1>
         <a className='github-project-one' href="https://github.com/marlybone/GoogleMapDataVis" target="_blank">
-        <FontAwesomeIcon icon={faGithub} style={githubStyle} />
+        <FontAwesomeIcon className='github-code' icon={faGithub} />
         </a>
         <div className='text-col-span-two'>
         <p>
           I utilized the Google Maps API and WorldBank Open Data's CSV information to craft a data visualization on Google Maps that showcases relevant country data. To enhance the user experience, I integrated Google's PlaceServices to allow for searching and identifying popular tourist destinations within cities.
         </p>
         </div>
-        <div className='image-two-col-span-two'><img src="src/img/weather01.jpg"/></div>
+        <div className='image-two-col-span-two'><Imagecycler className='places-proj' images={imagestwo}/></div>
         <h1 className='project-tags' style={{transform: 'translateX(-100%)', color:'white'}}>Weather App</h1>
         <a className='github-project-two' href="https://github.com/marlybone/weather-app" target="_blank">
-        <FontAwesomeIcon icon={faGithub} style={githubStyle} />
+        <FontAwesomeIcon className='github-code' icon={faGithub} />
         </a>
         <div className='text-two-col-span-two'>
          <p>
@@ -48,7 +48,7 @@ const Projects = () => {
         <div className='image-three-col-span-two'><img src="src/img/currcon01.jpg"/></div>
         <h1 className='project-tags' style={styles}>Currency Converter</h1>
         <a className='github-project-three' href="https://github.com/marlybone/CurrencyConverter" target="_blank">
-        <FontAwesomeIcon icon={faGithub} style={githubStyle} />
+        <FontAwesomeIcon className='github-code' icon={faGithub} />
         </a>
         <div className='text-three-col-span-two'>
          <p>
--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -1,40 +1,17 @@
 import React from 'react'
-import { useEffect , useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub , faTwitter , faCodepen , faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import useHoverIcons from './useHoverIcons'
 
 
 const Social = () => {
-  const icons = useRef(null);
-
-  useEffect(() => {
-    icons.current = document.querySelectorAll('.fontawesome-icons');
-    icons.current.forEach(icon => {
-      icon.addEventListener('mouseover',()=>{
-        icon.style.top = '-3px';
-      });
-      icon.addEventListener('mouseout',()=>{
-        icon.style.top = '0px';
-      });
-    });
-    return () => {
-      icons.current.forEach(icon => {
-        icon.removeEventListener('mouseover',()=>{
-          icon.style.top = '-3px';
-        });
-        icon.removeEventListener('mouseout',()=>{
-          icon.style.top = '0px';
-        });
-      });
-    };
-  }, []);
-    
+  useHoverIcons('.fontawesome-icons');
   return(
     <div className="side-container">
       <div className='side-items'>
-        <a href=""><FontAwesomeIcon icon={faLinkedin} className="fontawesome-icons"/></a>
-        <a href=""><FontAwesomeIcon icon={faTwitter} className="fontawesome-icons"/></a>
-        <a href=""><FontAwesomeIcon icon={faGithub} className="fontawesome-icons"/></a>
+        <a href="https://www.linkedin.com/in/marlon-stevenson-510738109/"><FontAwesomeIcon icon={faLinkedin} className="fontawesome-icons"/></a>
+        <a href="https://twitter.com/Maestvenlon"><FontAwesomeIcon icon={faTwitter} className="fontawesome-icons"/></a>
+        <a href="https://github.com/marlybone"><FontAwesomeIcon icon={faGithub} className="fontawesome-icons"/></a>
         <a href=""><FontAwesomeIcon icon={faCodepen} className="fontawesome-icons"/></a>
       </div>
     </div>
